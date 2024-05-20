a0f68c3b0ea3824cbb54abafe3fba580c4bb0e73
HyperRamzey <36453187+HyperRamzey@users.noreply.github.com>
2024-05-20 06:14:35 +0300

    Update v2.0.0
    This update includes major changes such as windows support.

--- mining.py
+++ mining.py
@@ -4,19 +4,9 @@ import platform
 import subprocess
 import shutil
 import multiprocessing
-import atexit
+import time
 
 CONFIG_FILE = 'config.ini'
-
-
-def exit_handler():
-    revert_fan_speed = input('Do you want to restore fan speed?: (y/n) ')
-    if revert_fan_speed.lower() == 'y':
-        lock_fan_control_command = "sudo nvidia-settings -a [gpu:0]/GPUFanControlState=0"
-        os.system(lock_fan_control_command)
-    else:
-        exit(1)
-
 
 def input_mining_address():
     print("Please go to the following link to find your mining address:")
@@ -103,52 +93,102 @@ def download_deps():
             subprocess.run(['7z', 'e', 'miniz.tar.gz'])
             subprocess.run(['7z', 'e', 'miniz.tar'])
             subprocess.run(['chmod', '+x', 'miniZ'])
-    if os.path.exists("xmrig-6.21.3"):
-        shutil.rmtree('xmrig-6.21.3')
-    if os.path.exists("miniZ.sha256sum"):
-        os.remove('miniZ.sha256sum')
-    if os.path.exists("SHA256SUMS"):
-        os.remove('SHA256SUMS')
-    if os.path.exists("xmrig-6.21.3-linux-static-x64.tar.gz"):
-        os.remove('xmrig-6.21.3-linux-static-x64.tar.gz')
-    if os.path.exists("xmrig-6.21.3-linux-static-x64.tar"):
-        os.remove('xmrig-6.21.3-linux-static-x64.tar')
-    if os.path.exists("miniz.tar.gz"):
-        os.remove('miniz.tar.gz')
-    if os.path.exists("miniz.tar"):
-        os.remove('miniz.tar')
+        if os.path.exists("xmrig-6.21.3"):
+            shutil.rmtree('xmrig-6.21.3')
+        if os.path.exists("miniZ.sha256sum"):
+            os.remove('miniZ.sha256sum')
+        if os.path.exists("SHA256SUMS"):
+            os.remove('SHA256SUMS')
+        if os.path.exists("xmrig-6.21.3-linux-static-x64.tar.gz"):
+            os.remove('xmrig-6.21.3-linux-static-x64.tar.gz')
+        if os.path.exists("xmrig-6.21.3-linux-static-x64.tar"):
+            os.remove('xmrig-6.21.3-linux-static-x64.tar')
+        if os.path.exists("miniz.tar.gz"):
+            os.remove('miniz.tar.gz')
+        if os.path.exists("miniz.tar"):
+            os.remove('miniz.tar')
+    if platform.system() == 'Windows':
+        if os.path.exists("miniZ.exe"):
+            print("miniz found")
+        else:
+            print("Password is miniZ")
+            print("Password is miniZ")
+            print("Password is miniZ")
+            time.sleep(2)
+            subprocess.run(['powershell', '-Command', 'iwr',
+                           'https://miniz.cc/?smd_process_download=1"&"download_id=5016', '-OutFile', '1.zip'])
+            subprocess.run(['7z', 'e', '1.zip'])
+        if os.path.exists("xmrig.exe"):
+            print("xmrig found")
+        else:
+            subprocess.run(['powershell', '-Command', 'iwr',
+                            'https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-msvc-win64.zip', '-OutFile', '2.zip'])
+            subprocess.run(['7z', 'e', '2.zip'])
+        if os.path.exists("xmrig-6.21.3"):
+            shutil.rmtree('xmrig-6.21.3')
+        if os.path.exists("miniZ.exe.sha256sum"):
+            os.remove('miniZ.exe.sha256sum')
+        if os.path.exists("SHA256SUMS"):
+            os.remove('SHA256SUMS')
+        if os.path.exists("miniZ_gui.exe"):
+            os.remove('miniZ_gui.exe')
+        if os.path.exists("miniZ_gui.exe.sha256sum"):
+            os.remove('miniZ_gui.exe.sha256sum')
+        if os.path.exists("1.zip"):
+            os.remove('1.zip')
+        if os.path.exists("2.zip"):
+            os.remove('2.zip')
+        if os.path.exists("pool_mine_example.cmd"):
+            os.remove('pool_mine_example.cmd')
+        if os.path.exists("rtm_ghostrider_example.cmd"):
+            os.remove('rtm_ghostrider_example.cmd')
+        if os.path.exists("solo_mine_example.cmd"):
+            os.remove('solo_mine_example.cmd')
+        if os.path.exists("start.cmd"):
+            os.remove('start.cmd')
+        if os.path.exists("benchmark_10M.cmd"):
+            os.remove('benchmark_10M.cmd')
+        if os.path.exists("benchmark_1M.cmd"):
+            os.remove('benchmark_1M.cmd')
 
 
 def run_xmrig(mining_address, worker_name, cpu_prio, protocol, mining_port, tls_flag):
-    xmrig_command = f'sudo ./xmrig -o stratum+{protocol}://randomxmonero.auto.nicehash.com:{mining_port} \
-    -u {mining_address}.{worker_name} -p x \
-    -k {tls_flag} --nicehash --coin monero -a rx/0 \
-    --cpu-priority={cpu_prio} --huge-pages-jit --asm=auto \
-    --randomx-cache-qos --randomx-1gb-pages --cpu-memory-pool=-1 \
-    --argon2-impl=AVX2 '
+    if platform.system() == 'Linux':
+        xmrig_command = f'sudo ./xmrig -o stratum+{protocol}://randomxmonero.auto.nicehash.com:{mining_port} \
+        -u {mining_address}.{worker_name} -p x \
+        -k {tls_flag} --nicehash --coin monero -a rx/0 \
+        --cpu-priority={cpu_prio} --huge-pages-jit --asm=auto \
+        --randomx-cache-qos --randomx-1gb-pages --cpu-memory-pool=-1 \
+        --argon2-impl=AVX2'
+    else:
+        xmrig_command = f'xmrig.exe -o stratum+{protocol}://randomxmonero.auto.nicehash.com:{mining_port} \
+                -u {mining_address}.{worker_name} -p x \
+                -k {tls_flag} --nicehash --coin monero -a rx/0 \
+                --cpu-priority={cpu_prio} --asm=auto \
+                --randomx-cache-qos --cpu-memory-pool=-1 \
+                --argon2-impl=AVX2'
     os.system(xmrig_command)
 
 
 def run_miniz(mining_alg, mining_address, worker_name, fan_speed, protocol, mining_port, powerlimit, gpuoffset, memoffset):
-    miniz_command = f'sudo ./miniZ --worker worker \
-    --url {protocol}://{mining_address}.{worker_name}@{mining_alg}.auto.nicehash.com:{mining_port} \
-    --oc1 --oc2 --ocX \
-    --smart-pers --fanspeed={fan_speed} --mt-auto --ocXsamples=1000 \
-    --gpuoffset={gpuoffset} --memoffset={memoffset} \
-    --power={powerlimit} --dag-fix'
+    if platform.system() == 'Linux':
+         miniz_command = f'sudo ./miniZ --worker {worker_name} \
+        --url {protocol}://{mining_address}.{worker_name}@{mining_alg}.auto.nicehash.com:{mining_port} \
+        --oc1 --oc2 --ocX \
+        --smart-pers --fanspeed={fan_speed} --mt-auto --ocXsamples=1000 \
+        --gpuoffset={gpuoffset} --memoffset={memoffset} \
+        --power={powerlimit} --dag-fix'
+    else:
+        miniz_command = f'miniZ.exe --worker {worker_name} \
+             --url {protocol}://{mining_address}.{worker_name}@{mining_alg}.auto.nicehash.com:{mining_port} \
+             --oc1 --oc2 --ocX \
+             --smart-pers --fanspeed={fan_speed} --mt-auto --ocXsamples=1000 \
+             --gpuoffset={gpuoffset} --memoffset={memoffset} \
+             --power={powerlimit} --dag-fix'
     os.system(miniz_command)
 
 
 def run_miner(mining_address, worker_name, use_cpu, use_gpu, tls_flag, cpu_prio, change_fan_speed, fan_speed, mining_alg, protocol, mining_port, powerlimit, gpuoffset, memoffset):
-    if change_fan_speed.lower() == 'y':
-        unlock_fan_control_command = "sudo nvidia-settings -a [gpu:0]/GPUFanControlState=1"
-        os.system(unlock_fan_control_command)
-        fan0_speed_command = f"sudo nvidia-settings -a [fan:0]/GPUTargetFanSpeed={
-            fan_speed}"
-        os.system(fan0_speed_command)
-        fan1_speed_command = f"sudo nvidia-settings -a [fan:1]/GPUTargetFanSpeed={
-            fan_speed}"
-        os.system(fan1_speed_command)
     if use_cpu.lower() == 'y':
         xmrig_process = multiprocessing.Process(
             target=run_xmrig, args=(mining_address, worker_name, cpu_prio, protocol, mining_port, tls_flag))
@@ -157,7 +197,6 @@ def run_miner(mining_address, worker_name, use_cpu, use_gpu, tls_flag, cpu_prio,
         miniz_process = multiprocessing.Process(
             target=run_miniz, args=(mining_alg, mining_address, worker_name, fan_speed, protocol, mining_port, powerlimit, gpuoffset, memoffset))
         miniz_process.start()
-
     if use_cpu.lower() == 'y':
         xmrig_process.join()
     if use_gpu.lower() == 'y':
@@ -166,7 +205,6 @@ def run_miner(mining_address, worker_name, use_cpu, use_gpu, tls_flag, cpu_prio,
 
 if __name__ == "__main__":
     download_deps()
-    atexit.register(exit_handler)
     mining_address, worker_name, use_cpu, use_gpu, tls_flag, cpu_prio, change_fan_speed, fan_speed, mining_alg, protocol, mining_port, powerlimit, gpuoffset, memoffset = load_from_config()
     if mining_address is None or worker_name is None or use_cpu is None or use_gpu is None or tls_flag is None or cpu_prio is None or change_fan_speed is None or fan_speed is None or mining_alg is None or protocol is None or mining_port is None or powerlimit is None or gpuoffset is None or memoffset is None:
         mining_address = input_mining_address()
